715ebfa6b5de538d6269582b682096c2ca9a473d
Scott Christensen <scottmc526@gmail.com>
2016-08-23 19:57:11 -0600

    landing page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-var app = angular.module('mySite', ['ngRoute']) 
+var app = angular.module('mySite', ['ngRoute'])
+
+app.config(function($routeProvider) {
+
+  $routeProvider
+  .when('/', {
+    templateUrl: 'partials/about.html'
+  })
+})
